aa3ac7a2f0eeb4f1f4ae4f05dd78ad0788b87393
sachinsahoo98 <51692440+sachinsahoo98@users.noreply.github.com>
2020-05-09 21:32:14 +0530

    initial commit

---
+++ library/admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; Library Automation And Management System |<a href="https://github.com/sachinsahoo98" target="_blank"  > Designed by :CSE_Batch2020 </a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
